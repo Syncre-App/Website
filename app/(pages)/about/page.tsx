d7bef1b6cbf354565751a472bfc79846e244dfa9
B3ni <ballobeni08@gmail.com>
2025-06-25 22:50:33 +0200

    Add About and Team pages with Navbar component

---
+++ app/(pages)/about/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Navbar from '../../components/Navbar';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+        <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-br from-gray-900 via-black to-gray-900 text-white'>
+            About
+      </main>
+    </>
+  );
+}
